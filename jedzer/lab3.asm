f2a876751da0174df40efa32738ef840fe2f88e0
jedzer <jenyamalina@tut.by>
2018-09-27 23:17:40 +0300

    Malinouski Eugene [LAB2]
    Same as in lab 2

--- jedzer/lab3.asm
+++ jedzer/lab3.asm
@@ -1,27 +1,26 @@
 model small
 stack 256
 dataseg
-    integer db 8 dup(?)
     errorNotDigit db  13, 10, 'Entered symbol is not in [0, 9] or number is not in [-32768..32767]', 13, 10, '$'
     a dw ?
     b dw ?
     ten dw 10
-    minusFlag dw 32768
 codeseg
 
+;number input porcedure. Reads number from console and saves it in AX
+;saves bx, cx, si 
 EnterInt proc
     push bx
     push cx
-    push dx
     push si
+    xor si, si
     mov cx, 0
-    mov si, 0
         mov ah, 01h             
         int 21h
         cmp al, '-'
-        je negativeLoop
         jne positiveCheck
-    positiveLoop:
+        inc si
+    ReadLoop:
         mov ah, 01h             
         int 21h
         positiveCheck:
@@ -36,39 +35,28 @@ EnterInt proc
         mov cx, ax
         mov ax, bx
         mul ten
-        add ax, cx
-        mov bx, ax
-        cmp ax, 0
-        jl intErr
-        jmp positiveLoop
-    negativeLoop:
-        mov ah, 01h             
-        int 21h
-        cmp al, 13
-        je finish
-        cmp al, '0'
-        jl intErr
-        cmp al, '9'
-        jg intErr
-        xor ah, ah
-        sub ax, '0'
-        mov cx, ax
-        mov ax, bx
-        mul ten
-        sub ax, cx
-        mov bx, ax
-        cmp ax, 0
-        jg intErr
-        jmp negativeLoop
+        cmp si, 0
+        jne IsNegative
+            add ax, cx
+            mov bx, ax
+            cmp ax, 0
+            jl intErr
+        jmp ReadLoop
+        IsNegative:
+            sub ax, cx
+            mov bx, ax
+            cmp ax, 0
+            jg intErr
+        jmp ReadLoop
 finish:
     mov ax, bx
     pop si
-    pop dx
     pop cx
     pop bx
     ret
 EnterInt endp
 
+;finishes programm if an input error occured  
 intErr:
     lea dx, errorNotDigit
     mov ah, 09h
@@ -76,51 +64,58 @@ intErr:
     mov ax, 4c00h
     int 21h
 
+;number output porcedure. Gets number from ax and outputs it in console
+;saves bx, cx, dx 
 OutInt proc
     push bx
     push cx
     push dx
-    push si
-    xor bx, bx
-    xor dx, dx
     xor cx, cx
-    xor si, si
     mov bx, 10
-    mov dx, ax
-    and dx, minusFlag
-    cmp dx, minusFlag
-    jne notDone 
-        mov integer[si], '-'
-        inc si
+    cmp ax, 0
+    jg SplitAndPush
+    je SplitAndPush 
+        push ax
+        mov dx, '-'
+        mov ah, 2   
+        int 21h
+        pop ax
         neg ax
-    notDone:
+    SplitAndPush:
         xor dx, dx
         div bx
         push dx
         inc cx
         test ax, ax
-        jnz notDone
-    finish2:
-        lopyLoop:
-            pop ax
-            add ax, '0'
-            mov integer[si], al 
-            inc si
-        loop lopyLoop
-        mov integer[si], 13
-        inc si
-        mov integer[si], 10
-        inc si
-        mov integer[si], '$'
-    lea dx, integer
-    mov ah, 09h
-    int 21h
-    pop si
+        jnz SplitAndPush
+    PopAndDisplay:
+        pop ax
+        add ax, '0'
+        mov dx, ax
+        mov ah, 2
+        int 21h
+    loop PopAndDisplay
     pop dx
     pop cx
     pop bx
     ret
 OutInt endp
+
+;procedure used to display new line symbol
+;saves ax and dx
+NewLine proc
+    push ax
+    push dx
+        mov dx, 10
+        mov ah, 2   
+        int 21h 
+        mov dx, 13
+        mov ah, 2   
+        int 21h 
+    pop dx
+    pop ax
+    ret
+NewLine endp
 
 main:
     mov ax, @data
@@ -128,14 +123,20 @@ main:
     call EnterInt 
     mov a, ax
     call OutInt
+    call NewLine
+
     call EnterInt 
     mov b, ax
     call OutInt
+    call NewLine
+
     xor dx, dx
     mov ax, a
     cwd
     idiv b
     call OutInt
+    call NewLine
+    
     mov ax, 4c00h
     int 21h
 end main
