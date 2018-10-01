d3ac0ffd307fa72b64c1677c59d9d0f717d6e425
gwyrwch <gwyrwch@mail.ru>
2018-10-01 18:49:31 +0300

    Full refactoring

--- gwyrwch/lw3.asm
+++ gwyrwch/lw3.asm
@@ -2,8 +2,6 @@
 .stack 256
 .data
 	__cnt_digits dw 0
-
-    a dw 65535
     ten dw 10
     s db "Incorrect input$"
     zero db "Division by zero$"
@@ -13,7 +11,7 @@ endl PROC
 	PUSH dx
 	PUSH ax
 
-	MOV dx, 0AH
+	MOV dl, 0AH
 	MOV ah, 02h
 	INT 21h
 	
@@ -22,11 +20,13 @@ endl PROC
 	RET
 endl ENDP
 
+;unsigned printf
 uprintf PROC
-	PUSH cx
-	MOV cx, 0
-	PUSH dx
 	PUSH ax
+	PUSH cx
+	PUSH dx
+
+	MOV cx, 0
 	MOV dx, 0 
 
 	division :
@@ -36,19 +36,13 @@ uprintf PROC
 	   	INC cx
 
 	    CMP ax, 0
-	    JZ true
-
-	    JMP division
-
-	true:
+	    JNZ division
 
 	CMP cx, 0
-	JZ zero_came
-	JMP non_zero_came
+	JNZ non_zero_came
 	
-	zero_came:
-		PUSH 0
-		MOV cx, 1
+	PUSH 0
+	MOV cx, 1
 	
 	non_zero_came:
 		POP dx
@@ -57,35 +51,32 @@ uprintf PROC
 		INT 21h
 	LOOP non_zero_came
 
-	POP ax
 	POP dx
 	POP cx
+	POP ax
 	RET
 uprintf ENDP
 
-printf PROC
+printf PROC 
 	PUSH bx
 	PUSH dx
 	PUSH ax
 
 	CWD
 	CMP dx, 0
+	JZ plus
 
-	JNZ minus
-	JMP plus
+	MOV bx, ax
 
-	minus:
-		MOV bx, ax
+	MOV dx, '-'
+	MOV ah, 02h
+	INT 21h
 
-		MOV dx, '-'
-		MOV ah, 02h
-		INT 21h
+	MOV ax, bx
+	NEG ax
 
-		MOV ax, bx
-		NEG ax
 	plus:
-
-	CALL uprintf
+		CALL uprintf
 
 	POP ax
 	POP dx
@@ -116,9 +107,8 @@ scanf PROC
 
 	negative:
 		MOV si, 1
-		MOV ax, 0
 	
-	lo_op:
+	reading:
 		MOV ah, 01h
 		INT 21h
 
@@ -127,12 +117,10 @@ scanf PROC
 		CMP al, 13
 		JZ good_input
 
-		CMP al, 48 ; '0' = 48 '9' = 57 //47
-
-		JC bad_input
+		CMP al, 48 ; '0' = 48 '9' = 57 
+		JC bad_input 
 
 		CMP al, 58
-
 		JNC bad_input
 
 		MOV cl, al ; bx = bx * 10 + al - 48
@@ -146,18 +134,20 @@ scanf PROC
 
 		SUB cl, 48
 		MOV ch, 0
-		ADD bx, cx
+		ADD bx, cx ; if overflow CF = 1
 
 		INC __cnt_digits
 
 		JC bad_input
-	JMP	lo_op
+	JMP	reading
+
 	good_input:
 		CMP __cnt_digits, 0
-		JZ lo_op
+		JZ scanf_prep 
 
 		MOV ax, bx
 		JMP validate_range
+
 	bad_input:
 		LEA dx, s
 		MOV ah, 09h
@@ -166,6 +156,7 @@ scanf PROC
 		MOV ax, 0
 		CALL endl
 		JMP scanf_prep
+
 	validate_range:
 		; ax > 32767 + si
 		; -[1, 2, 3, 4, 32768]
@@ -179,7 +170,7 @@ scanf PROC
 
 		JNZ scanf_finish
 		NEG ax
-		JMP scanf_finish
+
 	scanf_finish:
 		POP cx
 		POP bx
@@ -210,8 +201,19 @@ main:
     CMP cx, 0
     JZ div_by_zero
 
-    CWD
+    CMP ax, 8000h
+		JNZ save_division
+	CMP cx, 0FFFFh
+		JNZ save_division
 
+	LEA dx, s
+	MOV ah, 09h
+	INT 21h
+	JMP end_main
+	save_division:
+
+
+    CWD
     IDIV cx
 
     CALL printf
@@ -225,6 +227,6 @@ main:
 
 	end_main:
 	    MOV ax, 4c00h
-	    int 21h
+	    INT 21h
 
 end main
