a13b31451c45cd206b7184890de9fb1b44c97f6f
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2018-10-16 11:33:37 +0300

    Лабораторная 1 [Клишевич Илья]
    Вариант 8. Найти максимум между a, b, c, d и отнять от него минимум между этими числами.

---
+++ KlishevichIlya/lab1.asm
@@ -0,0 +1,74 @@
+.model small
+.stack 256
+.data
+a dw 2
+b dw 3
+c dw 4
+d dw 5
+
+.code
+main:
+mov ax, @data 
+mov ds, ax 
+mov es, ax 
+mov ax,a
+mov si,a
+
+cmp ax,a
+ja L1
+mov ax,a
+jmp next
+L1:
+mov ax,a
+mov si,a
+next:
+mov bx,b
+
+cmp ax,bx
+ja L2
+mov ax,bx
+jmp next1
+L2:cmp si,bx
+ja P0
+mov si,si
+jmp net
+P0:mov si,bx
+mov si,bx
+net:
+next1:
+mov cx,c
+cmp ax,cx
+ja L3
+mov ax,cx
+
+
+L3: mov ax,ax
+cmp cx,si
+ja P1
+mov si,cx
+jmp next2
+
+P1:mov si,si
+next2:
+mov dx,d
+cmp ax,dx
+ja L4
+mov ax,dx
+
+
+
+jmp next3
+
+L4: mov ax,ax
+cmp si,dx
+ja N
+mov si,si
+jmp next4
+N: mov si,dx
+next4:
+next3:
+sub ax,si
+mov ax,4c00h 
+int 21h
+end main
+
