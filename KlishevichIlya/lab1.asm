e78199a902ade9f1031ffc00ad779a393863fb43
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2018-10-29 17:49:13 +0300

    Update lab1.asm

--- KlishevichIlya/lab1.asm
+++ KlishevichIlya/lab1.asm
@@ -1,54 +1,41 @@
-.model small
-.stack 256
-.data
-a dw 2
-b dw 3
-c dw 4
-d dw 5
-.code
-main:
-mov ax, @data 
-mov ds, ax 
 mov es, ax 
 mov ax,a
 mov si,a
 mov bx,b
 cmp ax,bx
-ja L2
+ja ifakicker
 mov ax,bx
-jmp next1
-L2:cmp si,bx
-ja P0
+jmp gotoc
+ifakicker:cmp si,bx
+ja comparemin
 mov si,si
-jmp net
-P0:mov si,bx
-next1:
+comparemin:mov si,bx
+gotoc:
 mov cx,c
 cmp ax,cx
-ja L3
+ja ifkicker
 mov ax,cx
-L3: mov ax,ax
+ifkicker: mov ax,ax
 cmp cx,si
-ja P1
+ja ifminrest
 mov si,cx
-jmp next2
-P1:mov si,si
-next2:
+jmp gotod
+ifminrest:mov si,si
+gotod:
 mov dx,d
 cmp ax,dx
-ja L4
+ja ifamax
 mov ax,dx
-jmp next3
-L4: mov ax,ax
+jmp gotosplit
+ifamax: mov ax,ax
 cmp si,dx
-ja N
+ja ifdxmin
 mov si,si
-jmp next4
-N: mov si,dx
-next4:
-next3:
+jmp split
+ifdxmin: mov si,dx
+split:
+gotosplit:
 sub ax,si
 mov ax,4c00h 
 int 21h
 end main
-
