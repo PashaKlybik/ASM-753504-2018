e4b796bc73b3d56745152544c4f5b4f02f9e4d94
Artisanter <chyzhyk99@gmail.com>
2018-11-07 23:45:38 +0300

    lab 4 fix

--- Artisanter/lab4.asm
+++ Artisanter/lab4.asm
@@ -4,56 +4,54 @@
 .model small
 .stack 256
 .data
-	string db "aajd ad dfffd 4ofkLLpaec",10,13,'$'
-	flag db 0
-	buffer db 250 dup('$')
+    string db "aajd ad dfffd 4ofkLLpaec",10,13,'$'
+    flag db 0
+    buffer db 250 dup('$')
 .code
 
 proc process
-	push cx
-	push bx
-	push ax
-	
-	xor bx, bx
-	mov si, dx
-	mov di, dx
+    push cx
+    push bx
+    push ax
+    
+    xor bx, bx
+    mov si, dx
+    mov di, dx
 forChar:
-	mov al, [si]
-	mov cx, bx
-	repne scasb
-	je found
-	mov di, dx
-	add di, bx
-	mov [di], al
-	inc bx
-	
+    mov al, [si]
+    mov cx, bx
+    repne scasb
+    je found
+    mov di, dx
+    add di, bx
+    mov [di], al
+    inc bx  
 found:
-	inc si	
-	cmp byte ptr [si], '$'
-	je endProcess	
-	mov di, dx
-	jmp forChar
-	
-endProcess:	
-	mov [di], '$'
-	
-	pop ax
-	pop bx
-	pop cx
-	ret
+    inc si  
+    cmp byte ptr [si], '$'
+    je endProcess   
+    mov di, dx
+    jmp forChar
+    
+endProcess: 
+    mov [di], '$'   
+    pop ax
+    pop bx
+    pop cx
+    ret
 endp
 
 main:
     mov ax, @data
     mov ds, ax
-	mov es, ax
-	
-	lea dx, string
+    mov es, ax
+    
+    lea dx, string
     mov ah, 09h
-    int 21h		
-	call process
-	int 21h
-	
+    int 21h     
+    call process
+    int 21h
+    
     mov ax, 4c00h
     int 21h
 end main
