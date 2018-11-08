181dbd953b97a65964c2c158f2b0b7b388e9c5da
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2018-11-08 08:54:41 +0300

    Update lab1.asm

--- KlishevichIlya/lab1.asm
+++ KlishevichIlya/lab1.asm
@@ -7,47 +7,52 @@ c dw 4
 d dw 5
 .code
 main:
-mov ax, @data 
-mov ds, ax 
-mov es, ax 
-mov ax,a
-mov si,a
-mov bx,b
-cmp ax,bx
-ja ifakicker
-mov ax,bx
-jmp gotoc
-ifakicker:cmp si,bx
-ja comparemin
-mov si,si
-comparemin:mov si,bx
-gotoc:
-mov cx,c
-cmp ax,cx
-ja ifkicker
-mov ax,cx
-ifkicker: mov ax,ax
-cmp cx,si
-ja ifminrest
-mov si,cx
-jmp gotod
-ifminrest:mov si,si
-gotod:
-mov dx,d
-cmp ax,dx
-ja ifamax
-mov ax,dx
-jmp gotosplit
-ifamax: mov ax,ax
-cmp si,dx
-ja ifdxmin
-mov si,si
-jmp split
-ifdxmin: mov si,dx
-split:
-gotosplit:
-sub ax,si
-mov ax,4c00h 
-int 21h
-end main
+    mov ax, @data 
+    mov ds, ax 
+    mov es, ax 
+    
+    mov ax,a
+    mov si,a
+    mov bx,b
+    cmp ax,bx
+    ja ifAKicker
+    mov ax,bx
+    jmp goToC
+    ifAKicker:cmp si,bx
+    ja compareMin
+    mov si,si
+    compareMin:mov si,bx
+    
+    goToC:
+    mov cx,c
+    cmp ax,cx
+    ja ifKicker
+    mov ax,cx
+    ifKicker: mov ax,ax
+    cmp cx,si
+    ja ifMinRest
+    mov si,cx
+    jmp goToD
+    ifMinRest:mov si,si
+    
+    goToD:
+    mov dx,d
+    cmp ax,dx
+    ja ifAMax
+    mov ax,dx
+    jmp goToSplit
+    ifAMax: mov ax,ax
+    cmp si,dx
+    ja ifDxMin
+    mov si,si
+    jmp split
+    ifDxMin: mov si,dx
+    
+    split:
+    goToSplit:
+    sub ax,si
+    
+    mov ax,4c00h 
+    int 21h
+ end main
 
