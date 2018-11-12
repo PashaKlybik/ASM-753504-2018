f39e8c214dcc2c9c4a426e5719284ef9ceb64cef
secha_404 <39493557+sechko25@users.noreply.github.com>
2018-11-12 19:33:49 +0300

    Update lab1.asm

--- Sechko/lab1.asm
+++ Sechko/lab1.asm
@@ -2,50 +2,48 @@
 .stack 100h 
 
 .data
-a DW 1
-b DW 8
-c DW 11
-d DW 4
+    a DW 1
+    b DW 8
+    c DW 11
+    d DW 4
 
 .code
 
 Begin:
-mov ax,@data
-mov ds, ax 
+    mov ax,@data
+    mov ds, ax 
 
-mov ax,a
-mov bx,b
-mov cx,c
-mov dx,d
+    mov ax,a
+    mov bx,b
+    mov cx,c
+    mov dx,d
 
-cmp bx,ax
-JGE IfBmore
-
-cmp cx,ax
-JGE IfCmore
-
-cmp dx,ax
-JGE IfDmore
-jmp EXIT
+    cmp bx,ax
+    JGE IfBmore
+    cmp cx,ax
+    JGE IfCmore
+    cmp dx,ax
+    JGE IfDmore
+    jmp EXIT
 
 IfBmore:
-cmp cx,bx
-JGE IfCmore
-cmp dx,bx
-JGE IfDmore
-mov ax,b
-jmp EXIT
+    cmp cx,bx
+    JGE IfCmore
+    cmp dx,bx
+    JGE IfDmore
+    mov ax,b
+    jmp EXIT
 
 IfCmore:
-cmp dx,cx
-JGE IfDmore
-mov ax,c
-jmp EXIT
+    cmp dx,cx
+    JGE IfDmore
+    mov ax,c
+    jmp EXIT
 
 IfDmore:
-mov ax,d
+    mov ax,d
 
 EXIT:
-mov ax,4C00h
-int 21h
+    mov ax,4C00h
+    int 21h
 end Begin
