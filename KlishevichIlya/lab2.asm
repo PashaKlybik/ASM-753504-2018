b5162fea52cd945d6e8d15206d94bbd19a2e97a2
Ilya <43515223+KlishevichIlya@users.noreply.github.com>
2018-11-20 20:30:22 +0300

    Update lab2.asm

--- KlishevichIlya/lab2.asm
+++ KlishevichIlya/lab2.asm
@@ -1,4 +1,4 @@
-  .model small                  
+.model small                  
 .stack 16h                    
 .data  
 
@@ -21,7 +21,7 @@ u dw 0
 
 .code                        
 start: 
-	    mov ax, @data          
+	      mov ax, @data          
         mov ds, ax   
         mov ah,9
 
@@ -58,13 +58,13 @@ start:
         mov bx, b        
         div bx            
         mov x, ax 
-	    call outp 	; Вывод результата на экран
+	      call outp 	; Вывод результата на экран
    
-	    mov ax, 4c00h
-	    int 21h
+	      mov ax, 4c00h
+	      int 21h
    
 inpt proc 		    ; Процедура ввода с клавиатуры
-	     mov mult10,0001      
+	       mov mult10,0001      
       	 mov z,0              ;обнуление результата
          mov cx,10            ;основание системы счисления
        	 lea si,numfld-1      ;устанвока указателя на начало буфера
@@ -82,10 +82,10 @@ inpt proc 		    ; Процедура ввода с клавиатуры
        	 mov ax,mult10        ;загрузка в ax нового значения
        	 mul cx               ;умножение на 10
        	 mov mult10,ax        ;новое значение 
-	     dec bx               ;на след. разряд числа
+	       dec bx               ;на след. разряд числа
        	 jnz @@Loop           ;продолжаем цикл
-		 cmp z,0
-		 jbe Err
+		     cmp z,0
+		     jbe Err
        	 ret
 inpt  endp
  
@@ -101,20 +101,20 @@ outp proc 			             ; Процедура вывода на экран
         lea si, ascval+9         ; Установка указателя на конец массива
         mov ax, x                ; Результат х заносится в ах
 @@More:
-	    cmp ax,10                ; Результат сравнивается с 10
+	      cmp ax,10                ; Результат сравнивается с 10
         jb @@Less                ; Если меньше,то не надо преорбразовывать
-		xor dx,dx				 ; Очистка dx для деления
-		div cx					 ; Делим на 10
-		or dl,30h				 ; Остаток к ASCII
-		mov [si],dl
-		dec si
-		jmp @@More
+		    xor dx,dx				 ; Очистка dx для деления
+	    	div cx					 ; Делим на 10
+	    	or dl,30h				 ; Остаток к ASCII
+		    mov [si],dl
+		    dec si
+	    	jmp @@More
 @@Less:   
-       or al, 30h                ; Если остаток меньшу 10, то выводм на экран
-       mov [si], al             
-       lea dx, ascval            ; Адрес массива
-       mov ah, 9                
-       int 21h
-       ret                       ; Выход из п\программы
-outp endp               	     ; Конец процедуры
+        or al, 30h                ; Если остаток меньшу 10, то выводм на экран
+        mov [si], al             
+        lea dx, ascval            ; Адрес массива
+        mov ah, 9                
+        int 21h
+        ret                       ; Выход из п\программы
+ outp endp               	     ; Конец процедуры
 end start
