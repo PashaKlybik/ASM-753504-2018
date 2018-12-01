2843fa2da7397b90d558e1a0478c09a9bffcc276
jedzer <jenyamalina@tut.by>
2018-12-01 22:56:37 +0300

    Ohhhh... Yeahhhhhhh!

--- jedzer/lab6.asm
+++ jedzer/lab6.asm
@@ -22,64 +22,218 @@
 ; 2) Перекрыть девятую функцию прерывания 21h таким образом, чтобы в выводимой строке маленькие буквы заменялись большими, 
 ; а большие на маленькие.
 
-.MODEL Small
-.286
-.STACK 256
+CSEG segment 
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG 
+org        80h            ; по смещению 80h от начала PSP находятся:
+    cmdLength         db     ?       ; длина командной строки
+    cmdLine           db     ?       ; и сама командная строка
+org 100h 
+Start: 
+jmp Init 
  
-INTNO           =       21h             ; номер прерывания
+Int_21h_proc proc 
+    cmp ah,9              
+    je Ok_09 
+        jmp dword ptr cs:[Int_21h_vect] 
+    Ok_09: 
+        push dx 
+        push 	di
+        push 	si
+        push	es
+
+        push	ds
+        pop 	es
+        mov IsActive, 1
+
+        mov		di, dx
+        mov 	si, dx
+        Loopy:
+            lodsb
+            cmp 	al, '$'
+            je 		Finish
+            cmp 	al, 'a'
+            jl 		Next
+            cmp		al, 'z'
+            jg		Ignore
+            sub		al, 20h
+            jmp 	Ignore
+            Next:
+                cmp 	al, 'A'
+                jl		Ignore
+                cmp 	al, 'Z'
+                jg		Ignore
+                add 	al, 20h
+            Ignore:
+                stosb
+                jmp Loopy
+        Finish:
+        pushf                 
+        call dword ptr cs:[Int_21h_vect] 
+        pop 	es
+        pop 	si
+        pop 	di
+        pop dx                
+    iret                  
+    
+    
+int_21h_proc endp 
+
  
-.CODE
- 
-Handler:                                ; обработчик прерывания
-                cmp     ah,9
-                jne     OldHandler
- 
-                push    ds
-                push    cs
-                pop     ds
-				;
-                lea     dx,Message
-                ;
-				pushf
-                call    dword ptr cs:[OldAddr]
-                pop     ds
-                iret                    ; выход из прерывания
- 
-Message         DB      'Hello World!$'
- 
-OldHandler:     DB      0EAh            ; возвращаем управление прерыванию
-OldAddr         DW      ?,?
- 
-TSREnd          =       $
-ORG     $-4
- 
-;----------------------------------------------------------------------------;
- 
-Start:
- 
-                push    ds              ; PSP
- 
-                mov     ah,49h
-                mov     es,ds:[2Ch]
-                int     21h             ; освобождаем Environment
- 
-                mov     ax,3500h + INTNO
-                int     21h             ; получаем старый вектор
-                mov     OldAddr[0],bx
-                mov     OldAddr[2],es
- 
-                mov     ah,25h
-                lea     dx,Handler
-                push    cs
-                pop     ds              ; DS = CS
-                int     21h             ; устанавливаем новый
- 
-                pop     ax              ; PSP
-                push    cs
-                pop     dx
-                sub     dx,ax
-                add     dx,(TSREnd-Handler+15)/16
-                mov     ax,3100h
-                int     21h             ; остаёмся резидентно
- 
-END             Start
+Int_40h_proc proc 
+    cmp ah, 1              
+    je Ok_01 
+    cmp ah, 2
+    je Ok_02
+    
+    jmp dword ptr cs:[Int_40h_vect] 
+    
+    Ok_01: 
+        push dx 
+        push 	di
+        push 	si
+        push	es
+
+            mov ax,2540h 
+            mov dx, word ptr Int_40h_vect
+            int 21h 
+
+        pop 	es
+        pop 	si
+        pop 	di
+        pop dx                
+        iret  
+
+    Ok_02: 
+        mov al, 1
+        iret   
+    
+    
+int_40h_proc endp
+
+int_40h_empty proc
+    iret
+int_40h_empty endp
+
+
+IsActive db 0
+Int_21h_vect dd ?
+Int_40h_vect dd ?
+msgAlreadyInstalled db 'ERROR: Already installed',0Dh,0Ah,'$'
+msgCmdArgsErr db 'ERROR: Command line arguments are invalid',0Dh,0Ah,'$'
+msgNotInstalled db 'ERROR: Not installed',0Dh,0Ah,'$'
+msgUninstalled db 'SUCCESS: Uninstalled',0Dh,0Ah,'$'
+msgInstalled db 'SUCCESS: Installed',0Dh,0Ah,'$'
+
+Init: 
+    
+
+    cmp byte ptr cmdLength, 0
+    je ZeroArgs
+    cmp byte ptr cmdLength, 3
+    jne CmdError
+    cmp byte ptr cmdLine[1],'-'
+    jne CmdError
+    cmp byte ptr cmdLine[2],'d'
+    jne CmdError
+    
+    IsSlashD:
+        xor al, al
+        mov ah, 2
+        int 40h
+        cmp al, 0
+        jne ReturnPrev
+        lea dx, msgNotInstalled
+        jmp MessageAndExit
+
+        ReturnPrev:
+            mov ah, 1
+            int 40h
+
+            mov ah,35h 
+            mov al,40h 
+            int 21h 
+
+            mov word ptr Int_40h_vect,bx
+            mov word ptr Int_40h_vect+2,es 
+
+            mov ax, 2540h
+            lea dx, int_40h_empty
+            int 21h
+            
+            mov ah,35h 
+            mov al,21h 
+            int 21h 
+
+            mov word ptr Int_21h_vect, bx
+            mov word ptr Int_21h_vect+2, es 
+        
+            mov ax,2521h 
+            lea dx, Int_21h_proc
+            int 21h
+
+            lea dx, msgUninstalled
+            mov ah, 9
+            int 21h
+        
+            lea dx, Init 
+            int 27h 
+
+
+
+
+    ZeroArgs:
+        xor al, al
+        mov ah, 2
+        int 40h
+        cmp al, 0
+        je Install
+        lea dx, msgAlreadyInstalled
+        jmp MessageAndExit
+
+    CmdError:
+        lea dx, msgCmdArgsErr
+        jmp MessageAndExit
+
+
+    Install:
+        
+
+        mov ah,35h 
+        mov al,21h 
+        int 21h 
+
+        mov word ptr Int_21h_vect,bx
+        mov word ptr Int_21h_vect+2,es 
+        
+        mov ah,35h 
+        mov al,40h 
+        int 21h 
+
+        mov word ptr Int_40h_vect,bx
+        mov word ptr Int_40h_vect+2,es 
+
+        mov ax,2521h 
+        lea dx, Int_21h_proc    
+        int 21h 
+
+        mov ax,2540h 
+        lea dx, int_40h_proc    
+        int 21h 
+        
+        mov ah, 9
+        lea dx, msgInstalled
+        int 21h
+        
+        lea dx, Init 
+        int 27h 
+    
+    MessageAndExit:
+        mov ah, 9
+        int 21h
+        mov ax, 4c00h
+        int 21h
+
+
+
+    CSEG ends 
+end Start 
