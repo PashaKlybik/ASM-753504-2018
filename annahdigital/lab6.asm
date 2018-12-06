502ac1e99e43346aed2f047f9900e29e342d89b1
Annah <annatardream@gmail.com>
2018-12-06 06:27:26 +0300

    lab 6.

--- annahdigital/lab6.asm
+++ annahdigital/lab6.asm
@@ -1,26 +1,23 @@
 CSEG segment
-assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG	
-org 80h
-commandLineLength db ?
-commandLine db ?	
+assume cs:CSEG, ds:CSEG, es:CSEG, ss:CSEG    
 org 100h
 Start:
-	jmp Init	
+    jmp Init    
 ;residental part
-Int_21h_proc proc			;our procedure that replaces/controls standard 09h
-	cmp ah,9 				;Is it 09h?
-	je Is09h
-	jmp dword ptr cs:[Int_21h_vect] 	;if no, to the original int21h
-Is09h:		
+Int_21h_proc proc            ;our procedure that replaces/controls standard 09h
+    cmp ah,9                 ;Is it 09h?
+    je Is09h
+    jmp dword ptr cs:[Int_21h_vect]     ;if no, to the original int21h
+Is09h:        
  pushf
     push ax
     push cx
     push dx
     push si
     push di
-    push cs		
-    pop es			;ds moves into es
-	cld
+    push cs        
+    pop es            ;ds moves into es
+    cld
     mov si, dx
     stringProcessing:
         lodsb
@@ -28,7 +25,7 @@ Is09h:
         je stringHasEnded
         lea di, vowels
         mov cx, 10
-        repne scasb		;compare al with es:di
+        repne scasb        ;compare al with es:di
         je stringProcessing
         mov dx, [si-1]
         mov ah, 02h
@@ -55,18 +52,18 @@ int_21h_proc endp
 installMessage db "Succesfully installed.",10,13,'$'
 
 Init:
-	mov ah,35h			;function 35h 
-	mov al,21h			;saves vector of 21h to es(segment):bx(offset)
-	int 21h
-	mov word ptr Int_21h_vect,bx
-	mov word ptr Int_21h_vect+2,es
-	mov ax,2521h		;installing our handler on 21h 
-	mov dx,offset Int_21h_proc ;ds:dx = handler
-	int 21h
-	mov ah, 9
+    mov ah,35h            ;function 35h 
+    mov al,21h            ;saves vector of 21h to es(segment):bx(offset)
+    int 21h
+    mov word ptr Int_21h_vect,bx
+    mov word ptr Int_21h_vect+2,es
+    mov ax,2521h        ;installing our handler on 21h 
+    mov dx,offset Int_21h_proc ;ds:dx = handler
+    int 21h
+    mov ah, 9
     lea dx, installMessage
     int 21h 
-	mov dx,offset Init
-	int 27h		;makes it residental (init = last byte)
+    mov dx,offset Init
+    int 27h        ;makes it residental (init = last byte)
 CSEG ends
 end Start
