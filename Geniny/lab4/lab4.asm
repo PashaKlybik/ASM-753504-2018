559b854c137f20d9bcded682821a1529d5bcee09
Dmitriy <43481771+Geniny@users.noreply.github.com>
2018-12-07 15:38:15 +0300

    Update codestyle

--- Geniny/lab4/lab4.asm
+++ Geniny/lab4/lab4.asm
@@ -2,98 +2,95 @@
 .stack 256
 .data
     
-	endlSymbol db 13,10,'$'
-	firstStr db 39,40 dup('$')
-	secondStr db 20 dup('$')
-	strLength dw ?
+  endlSymbol db 13,10,'$'
+  firstStr db 39,40 dup('$')
+  secondStr db 20 dup('$')
+  strLength dw ?
 		
 .code
 
 PrintStr proc
 
-	push ax
-	○
-	mov ah,9 
-	int 21h
-	xor dx,dx
+  push ax
+
+  mov ah,9 
+  int 21h
+  xor dx,dx
+  
+  pop ax
 	
-	pop ax
-	
-	ret
+  ret
 PrintStr endp
 
 EnterStr proc
 
-    push ax
-    push dx
-	
-	xor ax,ax
-	xor dx,dx
-	
-    mov ah,0Ah
-    lea dx,firstStr
-    int 21h
-	xor ax,ax
-    mov al,[firstStr+1]           
-    mov strLength,ax   
-	
-    pop dx
-    pop ax
-	
-	ret
+  push ax
+  push dx
+  
+  xor ax,ax
+  xor dx,dx
+  mov ah,0Ah
+  lea dx,firstStr
+  int 21h
+  xor ax,ax
+  mov al,[firstStr+1]           
+  mov strLength,ax   
+  
+  pop dx
+  pop ax
+  
+  ret
 EnterStr endp
 
-	assume ds:@data,es:@data
+  assume ds:@data,es:@data
 main:
 
-	mov ax,@data
-	mov ds,ax 
-	mov es,ax 
+  mov ax,@data
+  mov ds,ax 
+  mov es,ax 
+  call EnterStr
+  xor ax,ax
+  lea dx, endlSymbol
+  call PrintStr
+
+  cld 
+  lea di,secondStr
+  lea si,firstStr
+  add si,2
+  lods firstStr
+  stos secondStr
+  dec di
+  dec si
+  xor ax,ax
+  mov cx,strLength
 	
-	call EnterStr
+  comparing:
+    cmps secondStr,firstStr
+      jcxz toExit
+      je equalSymbol
+      jne notEqualSymbol
+	  
+      equalSymbol:
+	dec cx
+	dec di 
+      jmp comparing
+	
+      notEqualSymbol:
+	dec cx
 	xor ax,ax
-	lea dx, endlSymbol
-	call PrintStr
-	
-	cld 
-	lea di,secondStr
-	lea si,firstStr
-	add si,2
+	dec si
 	lods firstStr
+	xor ah,ah
 	stos secondStr
 	dec di
-	dec si
-	
-	xor ax,ax
-	mov cx,strLength
-	
-comparing:
-    cmps secondStr,firstStr
-	  jcxz toExit
-	  je equalSymbol
-	  jne notEqualSymbol
-	  
-	equalSymbol:
-	  dec cx
-	  dec di 
-	jmp comparing
-	
-	notEqualSymbol:
-	  dec cx
-	  xor ax,ax
-	  dec si
-	  lods firstStr
-	  xor ah,ah
-	  stos secondStr
-	  dec di
-	jmp comparing
-	
-	toExit:
-	  lea dx,secondStr
-	  call PrintStr
-	  lea dx,endlSymbol
-	  call PrintStr
-	  mov ax,4c00h
-	  int 21h
+      jmp comparing
+      
+  toExit:
+    lea dx,secondStr
+    call PrintStr
+    lea dx,endlSymbol
+    call PrintStr
+    mov ax,4c00h
+    int 21h
 		
 end main
