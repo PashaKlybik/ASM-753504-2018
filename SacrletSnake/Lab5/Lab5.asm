60246869b901af151383e6588dc4bd5e1df067b5
ScarletSnake <43498440+ScarletSnake@users.noreply.github.com>
2018-12-08 10:44:36 +0300

    Create Lab5.asm

---
+++ SacrletSnake/Lab5/Lab5.asm
@@ -0,0 +1,300 @@
+.model small
+.stack 256
+
+.data
+	buffer1 db '       $'
+	buffer2 db '       $'
+	originalArray dw 10*10 dup (?)
+	sapceStr db '  $'
+	endline db 13,10,'$'
+
+.code
+Begin:
+	mov ax, @data
+    mov ds, ax
+
+	call InputNumber
+	call FunctionOfOutput
+	push ax
+	call InputNumber
+	call FunctionOfOutput
+	mov bx,ax
+	pop ax
+	call InputArray
+	call OutputArray
+	call OutputChangedArray
+
+	mov ax, 4c00h
+	int 21h
+
+FunctionOfOutput:
+	call ClearBuffer
+	call OutputWithSign
+	call EndStr
+	ret
+
+InputNumber:
+	push dx
+	mov al,7
+	call InputString
+	call StringToNumberWithSign
+	pop dx
+ret
+
+InputString:
+	push cx
+	mov cx,ax
+	mov ah,10
+	mov [buffer1],al
+	mov byte[buffer1+1],0
+	mov dx,offset buffer1
+	int 21h
+	mov al,[buffer1+1]
+	add dx,2
+	mov ah,ch
+	pop cx
+ret
+
+StringToNumberWithoutSign:
+	push cx
+	push dx
+	push bx
+	push si
+	push di	
+	mov si,dx
+	mov di,10
+	xor ah,ah
+	mov cx,ax
+	cmp cx,0
+	jz StdError
+	xor ax,ax
+	xor bx,bx
+Transform:
+	mov bl,[si]
+	inc si
+	cmp bl, '0'
+	jl StdError
+	cmp bl,'9'
+	jg StdError
+	sub bl,'0'
+	mul di
+	jc StdError
+	add ax,bx
+	jc StdError
+	loop Transform
+	jmp StdExit
+StdError:
+	xor ax,ax
+	stc
+StdExit:
+	pop di
+	pop si
+	pop bx
+	pop dx
+	pop cx
+	ret		
+
+StringToNumberWithSign:
+	push bx
+	push dx
+	test al,al
+	jz StdWithSignError
+	mov bx,dx
+	mov bl,[bx]
+	cmp bl,'-'
+	jne NoSign
+	inc dx
+	dec al
+NoSign:
+	call StringToNumberWithoutSign
+	jc StdWithSignExit
+	cmp bl,'-'
+	jne NumberPlus
+	cmp ax,32768
+	ja StdWithSignError
+	neg ax
+	jmp StdEnd
+NumberPlus:
+	cmp ax,32767
+	ja StdWithSignError
+StdEnd:
+	clc 
+	jmp StdWithSignExit
+StdWithSignError:
+	xor ax,ax
+	stc
+StdWithSignExit:
+	pop dx
+	pop bx
+	ret
+
+PrintString:
+	push ax
+	mov ah,9
+	xchg dx,di
+	int 21h
+	pop ax
+	ret
+
+EndStr:
+	push di
+	mov di,offset endline
+	call PrintString
+	pop di
+	ret
+
+SpaceStr:
+	push di
+	mov di,offset spaceStr
+	call PrintString
+	pop di
+	ret
+
+OutputString:
+	push ax
+	push cx
+	push bx
+	push dx
+	xor cx,cx
+	mov bx,10
+TransformToString:
+	xor dx,dx
+	div bx
+	add dl,'0'
+	push dx
+	inc cx
+	test ax,ax
+	jnz TransformToString
+IntoTheStack:
+	pop dx
+	mov [di],dl
+	inc di
+	loop IntoTheStack
+	pop dx
+	pop bx
+	pop cx
+	pop ax
+	ret
+
+OutputWithSign:
+	push ax
+	mov di,offset buffer2
+	test ax,ax
+	jns OutputWithoutSign
+	mov byte ptr[di],'-'
+	inc di
+	neg ax
+OutputWithoutSign:
+	call OutputString
+	mov di,offset buffer2
+	call PrintString
+	pop ax
+	ret
+
+ClearBuffer:
+	push cx
+	mov cx,7
+	lea dx,[buffer2]
+Clear:
+	mov byte ptr[di],' '
+	inc di
+	loop Clear
+	pop cx
+	ret
+
+InputArray:
+	push cx
+	mov cx,ax
+	push ax
+	mov si,0
+Input—olumnOfArray:
+	push cx
+	mov cx,bx
+	InputStringOfArray:
+		call InputNumber
+		mov originalArray[si],ax
+		call FunctionOfOutput
+		inc si
+		inc si
+		loop InputStringOfArray
+	pop cx
+	call EndStr
+	dec cx
+	cmp cx,0
+	jnz Input—olumnOfArray
+	pop ax
+	pop cx	
+	ret
+	
+	OutputArray:
+	push cx
+	mov cx,ax
+	push ax
+	mov si,0
+	mov ah,09h
+	lea dx,originalArray
+Output—olumnOfArray:
+	push cx
+	mov cx,bx
+	OutputStringOfArray:
+		mov ah,02h
+		mov dx,originalArray[si]
+		add dx,30h
+		int 21h
+		call SpaceStr
+		inc si
+		inc si
+		loop OutputStringOfArray
+	pop cx
+	call EndStr
+	dec cx
+	cmp cx,0
+	jnz Output—olumnOfArray
+	pop ax
+	pop cx
+	ret
+
+	OutputChangedArray:
+	call EndStr
+	push cx
+	mov cx,ax
+	push ax
+	call InputNumber
+	call FunctionOfOutput
+	call EndStr
+	push ax
+	mov si,0
+	mov ah,09h
+	pop ax
+	lea dx,originalArray
+OutputChanged—olumnOfArray:
+	push cx
+	mov cx,bx
+	OutputChangedStringOfArray:		
+		cmp originalArray[si],ax
+		jle Less
+		push ax
+		mov ax,0
+		mov originalArray[si],ax
+		pop ax
+	Less:
+		mov dx,originalArray[si]
+		add dx,30h
+		push ax
+		mov ah,02h
+		int 21h
+		pop ax
+		call SpaceStr
+		inc si
+		inc si
+		loop OutputChangedStringOfArray
+	pop cx
+	call EndStr
+	dec cx
+	cmp cx,0
+	jnz OutputChanged—olumnOfArray
+	pop ax
+	pop cx
+	ret
+
+end Begin
